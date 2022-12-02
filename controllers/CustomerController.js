d3b7a93819eefc96f2b580d0769e466f55c1fa0a
Rijo Biju <rijobijua.github@outlook.com>
2022-12-02 20:28:16 +0530

    requires userID for customer

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -5,7 +5,9 @@ export default {
     res.send("this route works");
   },
   post: async (req, res) => {
+    req.body["userId"] = req.user.id;
     const newCustomer = new Customer(req.body);
+    console.log(newCustomer);
     const savedCustomer = await newCustomer.save();
 
     res.json(savedCustomer);
--- routes/invoice.js
+++ routes/invoice.js
@@ -4,6 +4,7 @@ import controller from "../controllers/InvoiceController.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  console.log(req.user);
   controller.getAll(req, res);
 });
 
