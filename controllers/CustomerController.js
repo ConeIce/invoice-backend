283bb2700549c264b222eb10e1ad332cf849b299
Rijo Biju <rijobijua.github@outlook.com>
2022-12-03 22:25:41 +0530

    feat issue-4

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -3,21 +3,18 @@ import Customer from "../models/Customer.js";
 export default {
   getAll: async (req, res) => {
     const customers = await Customer.find({ userId: req.user.id });
-    res.send(customers);
+    res.json(customers);
   },
   getByID: async (req, res) => {
-    const query = Customer.where({ _id: req.params.id, userId: req.user.id });
-    query.findOne((err, customer) => {
-      if (err) throw err;
-      if (customer) {
-        res.send(customer);
-      }
-    });
+    const customer = await Customer.findOne({
+      _id: req.params.id,
+      userId: req.user.id,
+    }).exec();
+    res.json(customer);
   },
   post: async (req, res) => {
     req.body["userId"] = req.user.id;
     const newCustomer = new Customer(req.body);
-    console.log(newCustomer);
     const savedCustomer = await newCustomer.save();
 
     res.json(savedCustomer);
