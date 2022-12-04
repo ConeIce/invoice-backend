0a475d68139af8e6b5c8258b6b29bc923e2ba124
Rijo Biju <rijobijua.github@outlook.com>
2022-12-04 11:11:56 +0530

    feat issue-8

--- controllers/InvoiceController.js
+++ controllers/InvoiceController.js
@@ -1,8 +1,17 @@
 import Invoice from "../models/Invoice.js";
 
 export default {
-  getAll: (req, res) => {
-    res.send("this route works");
+  getAll: async (req, res) => {
+    const invoices = await Invoice.find();
+    res.json(invoices);
+  },
+  getByID: async (req, res) => {
+    await Invoice.findOne({ _id: req.params.id }, (err, invoice) => {
+      if (err) throw err;
+      if (invoice) {
+        res.json(invoice);
+      }
+    });
   },
   post: async (req, res) => {
     const newInvoice = new Invoice(req.body);
