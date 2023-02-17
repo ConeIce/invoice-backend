ce5c583d02071ef14e2400216ca1fbe6156044ce
Rijo Biju <rijobijua.github@outlook.com>
2023-02-17 23:56:22 +0530

    validate user registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 import express from "express";
 import controller from "../controllers/AuthController.js";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
@@ -7,8 +8,17 @@ router.post("/login", (req, res) => {
   controller.login(req, res);
 });
 
-router.post("/register", (req, res) => {
-  controller.register(req, res);
-});
+router.post(
+  "/register",
+  body("email").isEmail(),
+  body("password").isLength({ min: 5 }),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+    controller.register(req, res);
+  }
+);
 
 export default router;
