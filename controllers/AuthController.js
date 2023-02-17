9e021423b1d55457475c4217ee2f8aba7a597b29
Rijo Biju <rijobijua.github@outlook.com>
2023-02-17 23:38:34 +0530

    auth refactor

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,36 +2,65 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import passport from "passport";
 
-export default {
-  login: (req, res, next) => {
-    passport.authenticate("local", (err, user, info) => {
-      if (err) throw err;
-      if (!user) res.send("No User Exists");
-      else {
-        req.logIn(user, (err) => {
-          if (err) throw err;
-          res.send("Successfully Authenticated");
-        });
-      }
-    })(req, res, next);
-  },
-  register: async (req, res) => {
-    console.log(req.body);
+const handleLogin = (req, res, next) => {
+  try {
+    passport.authenticate("local", (err, user) => {
+      if (err) return next(err);
 
-    User.findOne({ username: req.body.username }, async (err, doc) => {
-      if (err) throw err;
-      if (doc) res.send("User Already Exists");
-      if (!doc) {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-        const newUser = new User({
-          username: req.body.username,
-          email: req.body.email,
-          password: hashedPassword,
-        });
-        await newUser.save();
-        res.send("User Created");
+      if (!user) {
+        res.send(401, { success: false, message: "Authentication failed" });
       }
+
+      req.login(user, (err) => {
+        if (err) return next(err);
+
+        res.status(200).send("Successfully Authenticated");
+      });
     });
-  },
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+const userExists = (username) => {
+  User.findOne({ username }, async (err, doc) => {
+    if (err) throw err;
+
+    if (doc) return true;
+
+    if (!doc) return false;
+  });
+};
+
+const hashPassword = async (password) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  return hashedPassword;
+};
+
+const saveUser = async (username, email, password) => {
+  const newUser = newUser({
+    username,
+    email,
+    password,
+  });
+
+  await newUser.save((err) => {
+    if (err) throw err;
+
+    res.send("User Created");
+  });
+};
+
+const registerUser = async (req, res, next) => {
+  if (userExists(req.body.username)) {
+    const hashedPassword = hashPassword(req.body.password);
+    saveUser(req.body.username, req.body.email, hashedPassword);
+  } else {
+    res.send("User already exists");
+  }
+};
+
+export default {
+  login: handleLogin,
+  register: registerUser,
 };
